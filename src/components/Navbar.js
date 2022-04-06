2a82a3db65a560314b1a118ff7885afee1056735
Philip Hinchsliff <philip.hinchsliff@yahoo.com>
2022-04-06 22:11:36 +0300

    migrated to react version 18 and added navbar links active classes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,14 @@
 //router
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 //images
 import logo from '../assets/shared/logo.svg';
-//hooks
-import { useNavigate } from "react-router-dom";
-
-
 
 const Navbar = () => {
 
     const navigate = useNavigate()
+    //i used this location path to toogle between navbar link active class
+    const location = useLocation()
 
     return (
         <div className="navbarContainer">
@@ -20,11 +19,11 @@ const Navbar = () => {
                 <div className="navbarLine"></div>
             </div>
             <ul className="navbarRight">
-                <Link to='/'><li className="navbarItem"><span className="navbarLinkNumber">00</span>home</li>
+                <Link to='/'><li className={`navbarItem ${ location.pathname === '/' ? 'activeLink' : '' }`}><span className="navbarLinkNumber">00</span>home</li>
                 </Link>
-                <Link to='/destination'><li className="navbarItem"><span className="navbarLinkNumber">01</span>destination</li></Link>
-                <Link to='/crew'><li className="navbarItem"><span className="navbarLinkNumber">02</span>crew</li></Link>
-                <Link to='/technology'><li className="navbarItem"><span className="navbarLinkNumber">03</span>technology</li></Link>
+                <Link to='/destination'><li className={`navbarItem ${ location.pathname === '/destination' ? 'activeLink' : '' }`}><span className="navbarLinkNumber">01</span>destination</li></Link>
+                <Link to='/crew'><li className={`navbarItem ${ location.pathname === '/crew' ? 'activeLink' : '' }`}><span className="navbarLinkNumber">02</span>crew</li></Link>
+                <Link to='/technology'><li className={`navbarItem ${ location.pathname === '/technology' ? 'activeLink' : '' }`}><span className="navbarLinkNumber">03</span>technology</li></Link>
             </ul>
         </div>
 
--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-ReactDOM.render(
+// react v17
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+
+// react v18 migration
+const container = document.getElementById('root');
+const rootContainer = ReactDOM.createRoot(container);
+rootContainer.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+  </React.StrictMode>
+)
