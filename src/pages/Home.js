6f0b714fdf219414db1e87a95b1502fa788cc513
Philip Hinchsliff <philip.hinchsliff@yahoo.com>
2022-04-07 09:27:10 +0300

    home page desktop complete

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,22 @@
 const Home = () => {
     return (
         <div className="homeContainer">
+            <div className="homeMain">
+                <div className="homeMainLeft">
+                    <div className="innerLeftContainer">
+                        <h2>so, you want to travel to</h2>
+                        <h1>space</h1>
+                        <p>Let's face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we'll give you a truly out of this world experience!</p>
+                    </div>
+                </div>
+                <div className="homeMainRight">
+                    <div className="outerCircle">
+                        <div className="innerCircle btnCircle">
+                            <p className="circleText">explore</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
