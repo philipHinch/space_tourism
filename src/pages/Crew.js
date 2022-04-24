35c680a4cc5f07802213fd4c867f364883e86411
Philip Hinchsliff <philip.hinchsliff@yahoo.com>
2022-04-24 13:18:12 +0300

    added og tags

--- src/pages/Crew.js
+++ src/pages/Crew.js
@@ -4,9 +4,11 @@ import PageTitle from "../components/PageTitle";
 
 const Crew = () => {
     return (
-        <div className="crewContainer">
-            <PageTitle number={'02'} text={'meet your crew'} crewPageTitle={true} />
-            <Swiper />
+        <div className="wrapper">
+            <div className="crewContainer">
+                <PageTitle number={'02'} text={'meet your crew'} crewPageTitle={true} />
+                <Swiper />
+            </div>
         </div>
     );
 }
